0d5fd5a0dc2f6132dd0a5d943e58129e0ecbdda1
Crowdbotics <engineering@crowdbotics.com>
2022-10-20 10:32:15 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Addresswyijekrclv
+
+admin.site.register(Addresswyijekrclv)
 
 # Register your models here.
--- backend/tst_tc980_btcykaptv_39537/wsgi.py
+++ backend/tst_tc980_btcykaptv_39537/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tst_tc980_btcykaptv_39537.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tst_tc980_btcykaptv_39537.settings")
 
 application = get_wsgi_application()
